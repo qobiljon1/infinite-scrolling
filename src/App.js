db309b4a0f8b778690867eca5cae8e9426df28c3
qobiljon1 <jumaboyevqobiljon17@gmail.com>
2022-07-18 17:06:23 +0500

    infiniteScroll project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useEffect, useState } from "react";
+import "./App.css";
+const PAGE_NUMBER = 1;
 function App() {
+  const [state, setState] = useState([]);
+  const [page, setPage] = useState(PAGE_NUMBER);
+
+  useEffect(() => {
+    fetch(
+      "https://api.instantwebtools.net/v1/passenger?page" + page + "&size=5"
+    )
+      .then((response) => response.json())
+      .then((json) => setState([...state, ...json.data]));
+  }, [page]);
+
+  function scrollToEnd() {
+    setPage(page + 1);
+  }
+  function scrollFunction(e) {
+    const { clientHeight, scrollTop, scrollHeight } = e.currentTarget;
+    console.log(Math.floor(scrollHeight - scrollTop) - clientHeight);
+    if (Math.floor(clientHeight + scrollTop) === scrollHeight) {
+      scrollToEnd();
+    }
+  }
+  if (state.length < 1) {
+    <h2>Loading...</h2>;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" onScroll={(e) => scrollFunction(e)}>
+      {state.length > 0 &&
+        state.map((item, index) => (
+          <div className="container" key={index}>
+            <h4>ID: {item._id}</h4>
+            <h4>ID: {item.name}</h4>
+            <h4>ID: {item.trips}</h4>
+          </div>
+        ))}
     </div>
   );
 }
